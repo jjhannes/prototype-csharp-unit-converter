156f789f2d6ed7b984dc16ddd94c34338f25f371
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-05-14 05:44:12 +0200

    create solution with projects tests & domain

---
+++ prototype-csharp-unit-converter-domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace prototype_csharp_unit_converter_domain;
+
+public class Class1
+{
+
+}
---
+++ prototype-csharp-unit-converter-tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace prototype_csharp_unit_converter_tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
